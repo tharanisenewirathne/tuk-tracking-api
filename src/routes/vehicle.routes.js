42fa14f4fbe75424f67ad33f0a11051e88d5604e
Tharani Senevirathna <tharaninimeshika17@gmail.com>
2026-05-03 01:55:21 +0530

    Updated for security layer

--- src/routes/vehicle.routes.js
+++ src/routes/vehicle.routes.js
@@ -3,10 +3,22 @@ const router = express.Router();
 
 const vehicleController = require("../controllers/vehicle.controller");
 
-// Create vehicle
-router.post("/", vehicleController.createVehicle);
+const verifyToken = require("../middlewares/auth.middleware");
+const authorizeRoles = require("../middlewares/role.middleware");
 
-// Get all vehicles
-router.get("/", vehicleController.getAllVehicles);
+// CREATE vehicle (only HQ + PROVINCIAL)
+router.post(
+    "/",
+    verifyToken,
+    authorizeRoles("HQ_ADMIN", "PROVINCIAL_OFFICER"),
+    vehicleController.createVehicle
+);
+
+// GET vehicles (all authenticated users)
+router.get(
+    "/",
+    verifyToken,
+    vehicleController.getAllVehicles
+);
 
 module.exports = router;
