f1cc0755135842a94487c1b815514f14ad7d6286
Prabhath Madhusanka <mprabhathm@email.com>
2026-05-03 02:28:41 +0530

    Updated for get methods with auth

--- src/controllers/vehicle.controller.js
+++ src/controllers/vehicle.controller.js
@@ -21,3 +21,31 @@ exports.getAllVehicles = (req, res) => {
         res.json(results);
     });
 };
+
+exports.getByDistrict = (req, res) => {
+    Vehicle.getByDistrict(req.params.district, (err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+};
+
+exports.getByProvince = (req, res) => {
+    Vehicle.getByProvince(req.params.province, (err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+};
+
+exports.getWithLatestLocation = (req, res) => {
+    Vehicle.getWithLatestLocation((err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+};
+
+exports.getDistrictWithLocation = (req, res) => {
+    Vehicle.getByDistrictWithLocation(req.params.district, (err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+};
