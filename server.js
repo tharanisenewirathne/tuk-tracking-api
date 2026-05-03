f20705a07ef84732fc30e295a7cd6c5765f19310
Tharani Senevirathna <tharaninimeshika17@gmail.com>
2026-05-03 16:36:40 +0530

    Deployment changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,15 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
     if (err) {
         console.error("Database connection failed:", err);
     } else {
-        console.log("Database connected successfully");
+        console.log("Connected to Railway MySQL");
     }
 });
 
